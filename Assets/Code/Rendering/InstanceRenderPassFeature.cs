38ff9616e5ce0974e7f0a8bc9875af376f802add
maajor <info@ma-yidong.com>
2024-06-06 10:53:02 +0800

    fix: pedestrian working but animation not working

--- Assets/Code/Rendering/InstanceRenderPassFeature.cs
+++ Assets/Code/Rendering/InstanceRenderPassFeature.cs
@@ -71,25 +71,16 @@ public class InstanceRenderPassFeature : ScriptableRendererFeature
             propertyParams.Dispose();
             _batcher.Dispose();
         }
-
-        // This method is called before executing the render pass.
-        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
-        // When empty this render pass will render to the active camera render target.
-        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
-        // The render pipeline will ensure target setup and clearing happens in a performant manner.
-        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-        {
-            _commandBuffer = cmd;
-        }
-
         // Here you can implement the rendering logic.
         // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
         // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
         // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            if (!inited || _commandBuffer == null) return;
+            if (!inited) return;
 
+            _commandBuffer = CommandBufferPool.Get("InstanceRenderPass");
+            _commandBuffer.name = "Instance";
 
             _batcher.Clear();
             UnityEngine.Profiling.Profiler.BeginSample("gather chunks");
@@ -114,12 +105,7 @@ public class InstanceRenderPassFeature : ScriptableRendererFeature
             UnityEngine.Profiling.Profiler.EndSample();
             context.ExecuteCommandBuffer(_commandBuffer);
             _commandBuffer.Clear();
-        }
-
-        // Cleanup any allocated resources that were created during the execution of this render pass.
-        public override void OnCameraCleanup(CommandBuffer cmd)
-        {
-            _commandBuffer = cmd;
+            CommandBufferPool.Release(_commandBuffer);
         }
 
         public void Render()
@@ -171,7 +157,7 @@ public class InstanceRenderPassFeature : ScriptableRendererFeature
         mPass = new InstanceRenderPass();
 
         // Configures where the render pass should be injected.
-        mPass.renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;
+        mPass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
     }
 
     // Here you can inject one or multiple render passes in the renderer.
