7857aa07b7a775440a77f3fb14c9bace90f9246f
maajor <info@ma-yidong.com>
2024-07-23 23:35:07 +0800

    misc: cleanup code

--- Assets/Code/Debug/LoadSceneUi.cs
+++ Assets/Code/Debug/LoadSceneUi.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
@@ -9,73 +7,100 @@ using UnityEngine;
 
 public class LoadSceneUi : MonoBehaviour
 {
-    public SceneOption[] scenes = new SceneOption[4]
+    public TMP_Dropdown Dropdown;
+    public RoadGraph RoadGraph;
+
+    public SceneOption[] scenes =
     {
-        new SceneOption()
+        new()
         {
             GeojsonPath = "Resources/Cities/Guangzhou/myd_guangzhou_2000.geojson",
             Name = "Guangzhou",
             Origin = new Vector2(113.37691f, 23.1387672f),
-            TerrainModelPath = "Cities/Guangzhou/Venue",
+            TerrainModelPath = "Cities/Guangzhou/Venue"
         },
-        new SceneOption(){
+        new()
+        {
             GeojsonPath = "Resources/Cities/RioDeJaneiro/myd_riodejaneiro_2000.geojson",
             Name = "RioDeJaneiro",
             Origin = new Vector2(-43.187042f, -22.9534571f),
-            TerrainModelPath = "Cities/RioDeJaneiro/Venue_Terrain",
+            TerrainModelPath = "Cities/RioDeJaneiro/Venue_Terrain"
         },
-        new SceneOption(){
+        new()
+        {
             GeojsonPath = "Resources/Cities/Seattle/myd_seattle_2000.geojson",
             Name = "Seattle",
             Origin = new Vector2(-122.33657f, 47.6221712f),
-            TerrainModelPath = "Cities/Seattle/Venue_Terrain",
+            TerrainModelPath = "Cities/Seattle/Venue_Terrain"
         },
-        new SceneOption(){
+        new()
+        {
             GeojsonPath = "Resources/Cities/Vienna/myd_vienna_2000.geojson",
             Name = "Vienna",
             Origin = new Vector2(16.416546f, 48.19311432f),
-            TerrainModelPath = "Cities/Vienna/Venue_Terrain",
+            TerrainModelPath = "Cities/Vienna/Venue_Terrain"
         }
     };
 
-    public TMP_Dropdown Dropdown;
-    public RoadGraph RoadGraph;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Dropdown.options = scenes.Select(option => new TMP_Dropdown.OptionData(option.Name)).ToList();
     }
 
+    /// <summary>
+    ///     For UI debug, binding it with UI Button to load from dropdown options
+    /// </summary>
     public void Load()
     {
-        for (int i = transform.childCount - 1; i >= 0; i--)
-        {
-            GameObject.Destroy(transform.GetChild(i).gameObject);
-        }
+        SceneOption scene = scenes[Dropdown.value];
+        LoadScene(scene);
+    }
 
-        var scene = scenes[Dropdown.value];
+    public void LoadScene(SceneOption scene)
+    {
+        CleanupPreviousScene();
 
-        var geoJsonAbsolutePath = Path.Join(Application.dataPath, scene.GeojsonPath);
-        string text = System.IO.File.ReadAllText(geoJsonAbsolutePath);
-        GeoJson rawData = JsonConvert.DeserializeObject<GeoJson>(text);
+        GeoJson rawData = LoadGeoJson(scene.GeojsonPath);
+        LoadAndSetupVenue(scene.TerrainModelPath);
 
-        var terrain = Resources.Load<GameObject>(scene.TerrainModelPath);
-        var terrainGameObject = Instantiate(terrain, transform);
-        terrainGameObject.transform.localScale = Vector3.one * 100.0f;
-        terrainGameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
-        var collider = terrainGameObject.AddComponent<MeshCollider>();
-        collider.sharedMesh = terrainGameObject.GetComponent<MeshFilter>().sharedMesh;
         // I have no idea why we need a cos latitude scale here, but it match the venue
         RoadGraph.Init(rawData, scene.Origin, Mathf.Cos(Mathf.Deg2Rad * scene.Origin.y));
     }
 
+    private void CleanupPreviousScene()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--) Destroy(transform.GetChild(i).gameObject);
+    }
 
+    private GeoJson LoadGeoJson(string geojsonPath)
+    {
+        string geoJsonAbsolutePath = Path.Join(Application.dataPath, geojsonPath);
+        string text = File.ReadAllText(geoJsonAbsolutePath);
+        return JsonConvert.DeserializeObject<GeoJson>(text);
+    }
+
+    private void LoadAndSetupVenue(string terrainModelPath)
+    {
+        GameObject terrain = Resources.Load<GameObject>(terrainModelPath);
+        GameObject terrainGameObject = Instantiate(terrain, transform);
+        terrainGameObject.transform.localScale = Vector3.one * 100.0f;
+        terrainGameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        MeshCollider meshCollider = terrainGameObject.AddComponent<MeshCollider>();
+        // setup collider for later raycast and get heights
+        meshCollider.sharedMesh = terrainGameObject.GetComponent<MeshFilter>().sharedMesh;
+    }
 }
 
 public struct SceneOption
 {
     public string Name;
+
     public Vector2 Origin;
+
+    /* need to start without Resources and has no extension, since we will load with Resources.Load */
     public string TerrainModelPath;
+
+    /* need to start with Resources and end with .geojson, since Unity does not recognize geojson as TextAsset, so we have to do File.ReadAllText */
     public string GeojsonPath;
 }
